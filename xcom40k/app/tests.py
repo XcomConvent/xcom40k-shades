6730df514d37a037d35cb4bbbb5e994d3b2a132c
marqueewinq <marqueewinq@gmail.com>
2015-10-17 23:30:14 +0300

    rewrite sample test

--- xcom40k/app/tests.py
+++ xcom40k/app/tests.py
@@ -33,29 +33,6 @@ class QuestionViewTests(TestCase):
     def test_try_to_ping(self):
         """ COMPONENT: app:*
         	ABOUT: Tries to ping up the server.
-        	EXPECTED: A '200 OK' response should be received from the server on pages:
-        			* app:index, 
-        			* app:login, 
-        			* app:missions, 
-        			* app:train. 
-        		A '302 REDIRECT' should be received on pages:
-        			* app:profile,
-        			* app:stash.
+        	EXPECTED: A '200 OK' response should be received from the server
         """
         self.assertEqual(self.client.get(reverse('app:index')).status_code,    200)
-        self.assertEqual(self.client.get(reverse('app:login')).status_code,    200)
-        self.assertEqual(self.client.get(reverse('app:missions')).status_code, 200)
-        self.assertEqual(self.client.get(reverse('app:train')).status_code,    200)
-
-        self.assertEqual(self.client.get(reverse('app:profile')).status_code,  302)
-        self.assertEqual(self.client.get(reverse('app:stash')).status_code,    302)
-
-    def test_root_is_present(self):
-        """ COMPONENT: *
-            ABOUT: Asserts the root user exists.
-            EXPECTED: A user with name 'root' with staff status = True should be found.
-        """
-        root_set = User.objects.filter(username = 'root')
-        self.assertEqual(len(root_set), 1)
-        self.assertEqual(root_set[0].is_staff, True)
-        self.assertEqual(root_set[0].is_superuser, True)
