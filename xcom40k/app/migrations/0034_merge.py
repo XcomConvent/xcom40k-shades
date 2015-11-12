34f417ecdcbdb8eb30fae5a1f1d6ea92eb8c3a58
marqueewinq <marqueewinq@gmail.com>
2015-11-12 11:51:20 +0300

    cleaned after dirty hands

---
+++ xcom40k/app/migrations/0034_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0032_auto_20151111_1723'),
+        ('app', '0033_auto_20151111_0520'),
+    ]
+
+    operations = [
+    ]
